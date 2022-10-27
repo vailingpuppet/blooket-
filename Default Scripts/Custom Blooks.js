e305a5e2cb250f09036f10f2ef881191d2b33ed8
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-10-27 16:11:17 -0400

    Update and rename Set Custom Blooks.js to Custom Blooks.js

--- Default Scripts/Custom Blooks.js
+++ Default Scripts/Custom Blooks.js
@@ -1,8 +1,19 @@
 var iframe = document.createElement('iframe');
 iframe.style.display = 'none';
 document.body.appendChild(iframe);
-window.prompt = iframe.contentWindow.prompt;
 window.alert = iframe.contentWindow.alert;
+window.prompt = iframe.contentWindow.prompt;
+
+var blooks = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner']
+	.stateNode.state.customBlooks;
+var split = "";
+for (i in blooks) {
+	split += `${blooks[i]}\n`;
+}
+var tab = window.open("about:blank", "_blank");
+tab.document.write("<textarea id=\"hack\" cols=150 rows=50></textarea>");
+tab.document.getElementById("hack").value = split;
+tab.document.close();
 
 var blook1 = prompt("Enter blook code 1. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
 var blook2 = prompt("Enter blook code 2. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
@@ -10,12 +21,12 @@ var blook3 = prompt("Enter blook code 3. (Nothing for blank):", "0#46#13#16#14$0
 var blook4 = prompt("Enter blook code 4. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
 var blook5 = prompt("Enter blook code 5. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
 
-Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner']
-	.stateNode.state.customBlooks = [
+Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner'].stateNode.state.customBlooks = [
 	blook1,
 	blook2,
 	blook3,
 	blook4,
 	blook5
 ];
-alert("Set blooks!");
+
+alert("Set custom blooks!");
