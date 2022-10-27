908130fadfeb05f025087b6ac8075fdbf4449d7a
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-10-27 16:17:45 -0400

    Update Custom Blooks.js

--- Default Scripts/Custom Blooks.js
+++ Default Scripts/Custom Blooks.js
@@ -4,17 +4,6 @@ document.body.appendChild(iframe);
 window.alert = iframe.contentWindow.alert;
 window.prompt = iframe.contentWindow.prompt;
 
-var blooks = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner']
-	.stateNode.state.customBlooks;
-var split = "";
-for (i in blooks) {
-	split += `${blooks[i]}\n`;
-}
-var tab = window.open("about:blank", "_blank");
-tab.document.write("<textarea id=\"hack\" cols=150 rows=50></textarea>");
-tab.document.getElementById("hack").value = split;
-tab.document.close();
-
 var blook1 = prompt("Enter blook code 1. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
 var blook2 = prompt("Enter blook code 2. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
 var blook3 = prompt("Enter blook code 3. (Nothing for blank):", "0#46#13#16#14$0#39#32#4#8#3#4$0");
