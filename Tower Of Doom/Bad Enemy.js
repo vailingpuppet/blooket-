168d2cdf65d2e3d66b501ea4157ae67db37bf65c
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-11-04 15:32:17 -0400

    Update Bad Enemy.js

--- Tower Of Doom/Bad Enemy.js
+++ Tower Of Doom/Bad Enemy.js
@@ -1,1 +1,14 @@
-var f = document.createElement('iframe');f.style.display = 'none';document.body.appendChild(f);window.prompt = f.contentWindow.prompt;(() => {  var hack = Object.values(document.querySelector('#app > div > div'))[1]    .children[1]['_owner'].stateNode.state;  if (hack.myCard) {    var u_prompt = window.prompt("What values do you want to set the enemy's card to?");    setInterval (function () {        hack.enemyCard.strength = u_prompt;        hack.enemyCard.wisdom = u_prompt;        hack.enemyCard.charisma = u_prompt;    });  }})();
+var iframe = document.createElement('iframe');
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
+window.prompt = iframe.contentWindow.prompt;
+
+var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner'].stateNode.state;
+if (hack.myCard) {
+  var u_prompt = window.prompt("What stats do you want to set the enemy's card to?");
+  setInterval(function() {
+    hack.enemyCard.strength = u_prompt;
+    hack.enemyCard.wisdom = u_prompt;
+    hack.enemyCard.charisma = u_prompt;
+  });
+}
