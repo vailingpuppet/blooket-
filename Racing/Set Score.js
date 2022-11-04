9f0e79afd2ee80c4ee875956064c5e42e1506549
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-11-04 15:29:05 -0400

    Update Set Score.js
    optimize

--- Racing/Set Score.js
+++ Racing/Set Score.js
@@ -1,1 +1,11 @@
-var i = document.createElement('iframe');i.style.display = 'none';document.body.appendChild(i);window.alert = i.contentWindow.alert;window.prompt = i.contentWindow.prompt;var world = Object.values(document.querySelector('#app > div > div'))[1]  .children[1]['_owner'].stateNode.state;var u_prompt = prompt("How many questions do you want to be at?");world.progress = world.goalAmount - u_prompt - 1;alert('Get the answer correct to set score');
+var iframe = document.createElement('iframe');
+iframe.style.display = 'none';
+document.body.appendChild(i);
+window.alert = iframe.contentWindow.alert;
+window.prompt = iframe.contentWindow.prompt;
+
+var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner'].stateNode.state;
+var u_prompt = prompt("How many questions do you want to be at?");
+
+hack.progress = world.goalAmount - u_prompt - 1;
+alert('Get the answer correct to set score');
