46b7137c998e9a67aa71b668c300263628c77f64
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-11-04 15:21:51 -0400

    Update Get Resources.js
    optimize

--- Crazy Kingdom/Get Resources.js
+++ Crazy Kingdom/Get Resources.js
@@ -1,22 +1,15 @@
 (async () => {
-    let n = document.createElement('iframe');
-    document.body.append(n);
-    window.alert = n.contentWindow.alert.bind(window);
-    window.prompt = n.contentWindow.prompt.bind(window);
-    window.confirm = n.contentWindow.confirm.bind(window);
-    n.remove();
+    var iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+    window.alert = iframe.contentWindow.alert;
 
-    function reactHandler() {
-        return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
-    };
-    if (window.location.pathname != '/kingdom') {
-        alert('You must be in a crazy kingdom game!');
-    } else {
-        reactHandler().stateNode.setState({
-            materials: 1e308,
-            people: 1e308,
-            happiness: 1e308,
-            gold: 1e308
-        });
-    };
+    var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
+    
+    hack.setState({
+        materials: 1e308,
+        people: 1e308,
+        happiness: 1e308,
+        gold: 1e308
+    });
 })();
