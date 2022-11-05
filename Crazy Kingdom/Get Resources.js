9b74ae715c9f2f76b061b825e5932a35d71b9003
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-11-05 14:56:09 -0400

    Update Get Resources.js

--- Crazy Kingdom/Get Resources.js
+++ Crazy Kingdom/Get Resources.js
@@ -1,15 +1,15 @@
-(async () => {
-    var iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    window.alert = iframe.contentWindow.alert;
+var iframe = document.createElement('iframe');
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
+window.alert = iframe.contentWindow.alert;
 
-    var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
-    
-    hack.setState({
-        materials: 1e308,
-        people: 1e308,
-        happiness: 1e308,
-        gold: 1e308
-    });
-})();
+var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
+
+hack.setState({
+    materials: 1e308,
+    people: 1e308,
+    happiness: 1e308,
+    gold: 1e308
+});
+
+alert("Set resources to max.");
