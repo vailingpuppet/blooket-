205e2389b3e0f2aa1542ddbdad659faf70a4c008
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-11-05 14:39:32 -0400

    Update Edit Lure Level.js
    optimize

--- Fishing Frenzy/Edit Lure Level.js
+++ Fishing Frenzy/Edit Lure Level.js
@@ -1,15 +1,13 @@
-(async () => {
-    var iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    window.alert = iframe.contentWindow.alert;
-    window.prompt = iframe.contentWindow.prompt;
+var iframe = document.createElement('iframe');
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
+window.alert = iframe.contentWindow.alert;
+window.prompt = iframe.contentWindow.prompt;
 
-    var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
-    
-    var lure = Number(parseFloat(prompt('What do you want to set your lure to? (1 - 5)'))) - 1;
-    hack.setState({
-        lure: lure < 0 ? 0 : lure > 4 ? 4 : lure
-    });
-    alert('Lure set!');
-})();
+var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
+
+var lure = Number(parseFloat(prompt('What do you want to set your lure to? (1 - 5)'))) - 1;
+hack.setState({
+    lure: lure < 0 ? 0 : lure > 4 ? 4 : lure
+});
+alert('Lure set!');
